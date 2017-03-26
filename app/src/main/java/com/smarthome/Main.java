96b2ecfd017a2f3aa389c1b3f03b48ad59601059
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-03-25 20:28:54 -0400

    Add javadoc for Main command-line params

--- app/src/main/java/com/smarthome/Main.java
+++ app/src/main/java/com/smarthome/Main.java
@@ -15,27 +15,73 @@ public class Main {
      * @param args <p>
      *             args[0]:
      *             <p>
-     *             Port Number
+     *             This server's port number
+     *             <p>
      *             <p>
      *             args[1]:
      *             <p>
-     *             Use "0" to start Gateway Server
+     *             Use "0" to start a Gateway Server
      *             <p>
-     *             Use "1" to start DB Server
+     *             Use "1" to start a DB Server
      *             <p>
-     *             Use "2" to start Sensor Server
+     *             Use "2" to start a Sensor Server
      *             <p>
-     *             Use "3" to start Device Server
+     *             Use "3" to start a Device Server
      *             <p>
-     *             Use "4" to start User Server
+     *             Use "4" to start an Entrant Server
+     *             <p>
      *             <p>
      *             args[2]:
-     *             <i>For Sensor:</i> The type of sensor, i.e. Temperature, Motion, Door
      *             <p>
-     *             <i>For Device:</i> The type of smart device, i.e. Bulb, Outlet
+     *             <i>For Sensor:</i>
      *             <p>
-     *             <i>For Entrant:</i> The type of entrant, i.e. User, Intruder
-     *             </p>
+     *             Use "0" for a Temperature Sensor
+     *             <p>
+     *             Use "1" for a Motion Sensor
+     *             <p>
+     *             Use "2" for a Door Sensor
+     *             <p>
+     *             <i>For Device:</i>
+     *             <p>
+     *             Use "0" for a Bulb
+     *             <p>
+     *             Use "1" for an Outlet
+     *             <p>
+     *             <i>For Entrant:</i>
+     *             <p>
+     *             Use "0" for a User
+     *             <p>
+     *             Use "1" for an Intruder
+     *             <p>
+     *             <i>For Gateway Server:</i>
+     *             <p>
+     *             DB Server's host
+     *             <p>
+     *             <p>
+     *             args[3]:
+     *             <p>
+     *             <i>For Gateway Server:</i>
+     *             <p>
+     *             DB Server's port number
+     *             <p>
+     *             <i>For Sensors, Devices and Entrants:</i>
+     *             <p>
+     *             Gateway Server's host
+     *             <p>
+     *             <p>
+     *             args[4]:
+     *             <p>
+     *             <i>For Sensors, Devices and Entrants only</i>:
+     *             <p>
+     *             Gateway Server's port number
+     *             <p>
+     *             <p>
+     *             args[5]:
+     *             <p>
+     *             <i>For Entrants only</i>:
+     *             <p>
+     *             {@code true} if the Entrant has an authorization beacon attached to her keychain;
+     *             {@code false} otherwise
      * @throws RemoteException Thrown when a Java RMI Exception occurs
      */
     public static void main(String[] args) throws RemoteException {
