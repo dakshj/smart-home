5ab0cbd1ee06247df4f3a5c6b08a80f453d8c1a5
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-03-31 15:48:59 -0400

    Add javadoc for methods in SensorServer

--- app/src/main/java/com/smarthome/ioT/IoTServer.java
+++ app/src/main/java/com/smarthome/ioT/IoTServer.java
@@ -18,8 +18,20 @@ public interface IoTServer {
     void setRegisteredIoTs(final Map<IoT, Address> registeredIoTs, final long senderLogicalTime)
             throws RemoteException;
 
+    /**
+     * Sets the synchronisationOffset of the sensor
+     *
+     * @param synchronizationOffset The value to which offset needs to be set
+     * @throws RemoteException Thrown when a Java RMI exception occurs
+     */
     void setSynchronizationOffset(final long synchronizationOffset) throws RemoteException;
 
+    /**
+     * Sends the current system time of the sensor
+     *
+     * @return The current system time
+     * @throws RemoteException Thrown when a Java RMI exception occurs
+     */
     default long getCurrentTime() throws RemoteException {
         return System.currentTimeMillis();
     }
